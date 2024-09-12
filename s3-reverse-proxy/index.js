e3505aaf0163bd1787e27c4989f7202383044660
Piyush <91911367+piyushmishra1416@users.noreply.github.com.>
2024-09-12 18:10:26 +0530

    chore: added a reverse proxy server

---
+++ s3-reverse-proxy/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const httpProxy = require("http-proxy");
+
+const app = express();
+const PORT = 8000;
+const proxy = httpProxy.createProxy();
+
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
+const BASE_PATH = process.env.BASE_PATH;
+
+app.use((req, res) => {
+  const hostname = req.hostname;
+  const subdomain = hostname.split(".")[0];
+  const resolvesTo = `${BASE_PATH}/${subdomain}`;
+
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+proxy.on("proxyReq", (proxyReq, req, res) => {
+  const url = req.url;
+  if (url === "/") proxyReq.path += "index.html";
+});
+
+app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`));
